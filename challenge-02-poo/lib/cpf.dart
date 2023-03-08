2253bdcea236fad30b724dabc3758fe346eadb21
Juan Carlos <jcguitar1994@gmail.com>
2023-03-08 20:41:20 -0300

    Refactor flag to partCpf length

--- challenge-02-poo/lib/cpf.dart
+++ challenge-02-poo/lib/cpf.dart
@@ -3,8 +3,11 @@
 /// O atributo privado [_cpf] do tipo [String] representa o CPF.
 /// O método [validate] valida se o CPF é válido.
 /// O método [_calculateDigit] calcula os dígitos verificadores.
+
 class CPF {
   final String _cpf;
+
+  final _validCpfLength = 11;
 
   String get value =>
       _cpf.replaceAllMapped(RegExp(r'(\d{3})(\d{3})(\d{3})(\d{2})'), (match) {
@@ -22,9 +25,7 @@ class CPF {
   /// Valida se o CPF informado é válido.
   /// Retorna um [true] se for válido e [false] caso contrário.
   bool validate() {
-    const validCpfLength = 11;
-
-    if (_cpf.length != validCpfLength) return false;
+    if (_cpf.length != _validCpfLength) return false;
 
     final firstPartCpf = _cpf.substring(0, 9);
     final secondPartCpf = _cpf.substring(9, 11);
@@ -32,27 +33,26 @@ class CPF {
     final firstDigit = _calculateDigit(partCpf: firstPartCpf).toString();
 
     final secondDigit =
-        _calculateDigit(partCpf: firstPartCpf + firstDigit, isFirstDigit: false)
+        _calculateDigit(partCpf: firstPartCpf + firstDigit)
             .toString();
 
     return secondPartCpf == (firstDigit + secondDigit);
   }
 
   /// Calcula os dígitos verificadores do CPF.
-  /// Recebe a [partCpf] para cálculo do dígito e a flag [isFirstDigit] para
-  /// indicar se é o primeiro ou segundo dígito.
+  /// Recebe a [partCpf] para cálculo do dígito.
   /// Retorna um inteiro representando o dígito verificador.
-  int _calculateDigit({required String partCpf, bool isFirstDigit = true}) {
+  int _calculateDigit({required String partCpf}) {
     int multiplier = 2;
     int sum = 0;
-    int maxIndex = isFirstDigit ? 8 : 9;
+    int maxIndex = partCpf.length - 1;
 
     for (var i = maxIndex; i >= 0; i--) {
-      sum += (int.parse(partCpf[i]) * multiplier++);
+      sum += int.parse(partCpf[i]) * multiplier++;
     }
 
-    final rest = sum % 11;
+    final rest = sum % _validCpfLength;
 
-    return (rest < 2) ? 0 : (11 - rest);
+    return (rest < 2) ? 0 : (_validCpfLength - rest);
   }
 }
--- challenge-02-poo/lib/main.dart
+++ challenge-02-poo/lib/main.dart
@@ -1,7 +1,8 @@
 import 'cpf.dart';
 
 void main() {
-  final cpf = CPF(cpf: '280.012.389-38');
+  // final cpf = CPF(cpf: '280.012.389-38');
+  final cpf = CPF(cpf: '117.284.723-18');
 
   final isValidCpf = cpf.validate();
 
