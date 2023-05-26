eb0aed2ff661d861c46a7702e373c9821affabe9
Juan Carlos <jcguitar1994@gmail.com>
2023-05-26 19:04:14 -0300

    Revert "Add implicit animation exercise 1"
    This reverts commit 9a02374835a3ad4d4cd225c93239ea0236ae0c34.

--- implicit_animation_ex1/android/app/src/main/kotlin/com/example/implicit_animation_ex1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.implicit_animation_ex1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- implicit_animation_ex1/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:implicit_animation_ex1/app/app_widget.dart';
-
-void main() {
-  runApp(const AppWidget());
-}
