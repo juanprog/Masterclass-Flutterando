e6fe07df6a1663ad03eb6e7e0c97ff8397d207b4
Juan Carlos <jcguitar1994@gmail.com>
2023-05-26 18:55:15 -0300

    Add implicit animation exercise 2

---
+++ implicit_animation_ex2/android/app/src/main/kotlin/com/example/implicit_animation_ex2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.implicit_animation_ex2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ implicit_animation_ex2/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:implicit_animation_ex2/app/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
